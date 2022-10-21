6a1a750e79cd7b5784a97476eb54ab01a0e0c70a
victor <victor-brambilla@hotmail.com>
2022-10-21 19:30:29 -0400

    feat: Create ac control

---
+++ pages/api/turnOff.ts
@@ -0,0 +1,24 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+  name: string
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  fetch('https://46f1-177-65-158-83.sa.ngrok.io/on?password=190899',{
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({username:'sa'})
+  })
+  .then((response) => response.json())
+  .then((data) => res.status(200));
+  return res.send({
+      name: 'ok'
+    });
+}
